ad782c5af42136b7f554531a57f4d2cbd9197301
TYPEmber <nono02200059@gmail.com>
2022-12-24 00:47:04 +0800

    test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use ndarray_conv::conv_2d::fft::Conv2DFftExt;
+
 fn main() {
     use ndarray::prelude::*;
     use ndarray_conv::conv_2d::*;
@@ -6,17 +8,19 @@ fn main() {
     use std::time::Instant;
 
     let mut small_duration = 0u128;
-    let test_cycles_small = 100;
+    let test_cycles_small = 1;
     // small input images
     for _ in 0..test_cycles_small {
-        let x = Array::random((2000, 5000), Uniform::new(0., 1.));
-        let k = Array::random((21, 41), Uniform::new(0., 1.));
+        let x = Array::random((2000, 5000), Uniform::new(0f32, 1.));
+        let k = Array::random((21, 41), Uniform::new(0f32, 1.));
 
         let now = Instant::now();
         // x.conv_2d(&k);
-        ndarray_conv::conv_2d::fft::conv_2d::<f64, ndarray::OwnedRepr<f64>, ndarray::OwnedRepr<f64>>(
-            &x, &k,
-        );
+        // naive_conv::conv_2d(&x, &k);
+        x.conv_2d_fft(&k);
+        // ndarray_conv::conv_2d::fft::conv_2d::<f64, ndarray::OwnedRepr<f64>, ndarray::OwnedRepr<f64>>(
+        //     &x, &k,
+        // );
         // ndarray_conv::conv_2d::ndrustfft::conv_2d::<f64, ndarray::OwnedRepr<f64>, ndarray::OwnedRepr<f64>>(
         //     &x, &k,
         // );
